83ecc5af3f9469901b57a55ac72dc47aaa6e5c66
Drejtovit <drejtarron@gmail.com>
2025-01-31 22:09:25 +0100

    flightRepository and flight classes created

--- models/arrivals.php
+++ models/arrivals.php
@@ -3,8 +3,8 @@ class Arrivals extends Flights{
     private $id_arrival;
     private $takeoff_time;
 
-    function __construct($flight_id, $time, $origin,$destination,$airline,$status,$date,$id_arrival,$takeoff_time){
-        Flights::__construct($flight_id, $time, $origin,$destination,$airline,$status,$date);
+    function __construct($flight_id, $time, $origin,$airline,$status,$date,$id_arrival,$takeoff_time){
+        Flights::__construct($flight_id, $time, $origin,"Kosovo",$airline,$status,$date);
         $this->id_arrival = $id_arrival;
         $this->takeoff_time = $takeoff_time;
     }
--- repository/flightsRepository.php
+++ repository/flightsRepository.php
@@ -14,79 +14,140 @@ class userRepository{
     }
 
 
-    function insertFlight($user) {
+    function insertFlight($flight) {
         $conn = $this->connection;
 
-   
-        $name = $user->getName();
-        $email = $user->getEmail();
-        $password = $user->getPassword();
+        if($flight instanceof Arrivals){
 
-   
-        $sql = "INSERT INTO user ( name, email, password) VALUES (:name,:email,:password)";
+            $flight_id = $flight->getFlight_id();            
+            $time =  $flight->getTime();      
+            $origin =  $flight->getOrigin();
+            $destination =  $flight->getDestination();  
+            $airline =  $flight->getAirline();
+            $status =  $flight->getStatus();
+            $date =  $flight->getDate();
+            $takeoff_time=$flight->getTakeoffTime();
 
-        $conn->query($sql,[
-            ':name'=>$name,
-            ':email'=>$email,
-            ':password'=>password_hash($password, PASSWORD_BCRYPT)
+            $sql = "INSERT INTO arrivals (time,origin,destination,airline,flight_id,status,date,takeoff_time) VALUES (:time,:origin,:destination,:airline,:flight_id,:status,:date,:landing_time)";
+
+            $conn->query($sql,[
+                ':time'=>$time,
+                ':origin'=>$origin,
+                ':destination'=>$destination,
+                ':airline'=>$airline,
+                ':flight_id'=>$flight_id,
+                ':status'=>$status,
+                ':date'=>$date,
+                ':takeoff_time'=>$takeoff_time
+            ]);
+
+            echo "<script> alert('User has been inserted successfully!'); </script>";//fix
+
+        }else{
+
+            $flight_id = $flight->getFlight_id();            
+            $time =  $flight->getTime();      
+            $origin =  $flight->getOrigin();
+            $destination =  $flight->getDestination();  
+            $airline =  $flight->getAirline();
+            $status =  $flight->getStatus();
+            $date =  $flight->getDate();
+            $landing_time =$flight-> getLandingTime();
+
+            $sql = "INSERT INTO departures (time,origin,destination,airline,flight_id,status,date,landing_time) VALUES (:time,:origin,:destination,:airline,:flight_id,:status,:date,:landing_time)";
             
-        ]);
+            $conn->query($sql,[
+                ':time'=>$time,
+                ':origin'=>$origin,
+                ':destination'=>$destination,
+                ':airline'=>$airline,
+                ':flight_id'=>$flight_id,
+                ':status'=>$status,
+                ':date'=>$date,
+                ':landing_time'=>$landing_time
+            ]);
 
-       
-        echo "<script> alert('User has been inserted successfully!'); </script>";//fix
+            echo "<script> alert('User has been inserted successfully!'); </script>";//fix
+        }
+
+   
     }
 
-    function getAllUsers() {
+    function getAllFlights($flight_Type) { //returns flights by the type we send
         $conn = $this->connection;
 
-        $sql = "SELECT * FROM user";
+        $sql = "SELECT * FROM $flight_Type";
 
         
-        $users = $conn->query($sql)->fetchAll(); 
+        $flights = $conn->query($sql)->fetchAll(); 
 
-        return $users; 
+        return $flights; 
     }
 
     
-    function getUserById($id) {
+    function getFlightById($flight_Type,$flight_id) {
         $conn = $this->connection;
 
      
-        $sql = "SELECT * FROM user WHERE id='$id'";
+        $sql = "SELECT * FROM $flight_Type WHERE flight_id='$flight_id'";
 
         
-        $user = $conn->query($sql)->fetch(); 
+        $flight = $conn->query($sql)->fetch(); 
 
-        return $user;
+        return $flight;
     }
 
-    
-    function updateUser($id, $name,  $email,  $password) {
+
+    function updateFlightDepartures($flight_id,$time,$origin,$destination,$airline,$status,$date,$landing_time){
+        $conn = $this->connection;
+
+        $sql = "INSERT INTO departures (time,origin,destination,airline,flight_id,status,date,landing_time) VALUES (:time,:origin,:destination,:airline,:flight_id,:status,:date,:landing_time)";
+        
+        $conn->query($sql,[
+            ':time'=>$time,
+            ':origin'=>$origin,
+            ':destination'=>$destination,
+            ':airline'=>$airline,
+            ':flight_id'=>$flight_id,
+            ':status'=>$status,
+            ':date'=>$date,
+            ':landing_time'=>$landing_time
+            ]);
+
+        echo "<script>alert('Update was successful');</script>";
+    }
+
+
+    function updateFlightArrivals($flight_id, $time, $origin,$destination,$airline,$status,$date,$takeoff_time) {
         $conn = $this->connection;
 
 
-        $sql = "UPDATE user SET name=:name,  email=:email, password=:password WHERE id=:id";
- 
+        $sql = "INSERT INTO arrivals (time,origin,destination,airline,flight_id,status,date,takeoff_time) VALUES (:time,:origin,:destination,:airline,:flight_id,:status,:date,:landing_time)";
+
         $conn->query($sql,[
-            ':id'=>$id,
-            ':name'=>$name,
-            ':email'=>$email,
-            ':password'=>password_hash($password, PASSWORD_BCRYPT)
-            
-        ]);
+            ':time'=>$time,
+            ':origin'=>$origin,
+            ':destination'=>$destination,
+            ':airline'=>$airline,
+            ':flight_id'=>$flight_id,
+            ':status'=>$status,
+            ':date'=>$date,
+            ':takeoff_time'=>$takeoff_time
+            ]);
+
 
         echo "<script>alert('Update was successful');</script>";
     }
 
     
-    function deleteUser($id) {
+    function deleteUser($flight_Type,$flight_id) {
         $conn = $this->connection;
 
       
-        $sql = "DELETE FROM user WHERE id=:id";
+        $sql = "DELETE FROM $flight_Type WHERE flight_id=:flight_id";
 
         $conn->query($sql,[
-            ':id'=>$id
+            ':flight_id'=>$flight_id
         ]); 
 
         echo "<script>alert('Delete was successful');</script>";
